44b2a0f8a70618b4ac50cd90c67031a4e993ef8e
nthigamac <dmdmasha28@gmail.com>
2017-09-04 08:32:22 +0300

    change json entry point and server for deployment

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ var express = require('express'),
     app     = express();
 
 app.use(express.static('public'));
+var port = process.env.PORT || 3000;
 
 
 
-app.listen(3000, function(){
+app.listen(port, function(){
   console.log('React server is up');
 })
