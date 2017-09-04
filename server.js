63ddf4621bfa266ecd63bb95a00668b4edd634d8
nthigamac <dmdmasha28@gmail.com>
2017-09-04 08:44:26 +0300

    prepare api calls for heroku

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 var express = require('express'),
     app     = express();
 
+app.use(function(req, res, next){
+  if(req.headers['x-forwarded-proto' === 'http']){
+    next();
+  }else{
+    res.redirect('http://' + req.hostname + req.url); 
+  }
+
+});
+
+const PORT = process.env.PORT || 3000;
 app.use(express.static('public'));
-var port = process.env.PORT || 3000;
 
 
 
-app.listen(port, function(){
+app.listen(PORT, function(){
   console.log('React server is up');
 })
